4e8eed6c3e0da0b00bd580bbc451226de507e79b
Gerald Oster <oster@loria.fr>
2013-10-03 13:36:26 +0200

    switched to java6 menu

--- src/eu/telecomnancy/Client.java
+++ src/eu/telecomnancy/Client.java
@@ -24,44 +24,16 @@ public class Client {
         while (!"q".equals(rep)) {
             try {
                 rep= c.nextLine();
-                switch (rep) {
-                    case "o": {
+                if ("o".equals(rep))
                         sense.onOff();
-                        break;
-                    }
-                    case "s": {
+                else if ("s".equals(rep))
                         System.out.println("status :"+sense.getStatus());
-                        break;
-                    }
-                    case "v": {
+                else if ("v".equals(rep))
                         System.out.println("value :"+sense.getValue());
-                        break;
-                    }
-                    default : System.out.println("q: quitter - o: switch - s: status - v: value");
-                }
+                else System.out.println("q: quitter - o: switch - s: status - v: value");
             } catch (SensorNotActivated ex) {
                 Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
             }
         }
     }
-    
-//   Code pour JDK 1.6
-//
-//       public void menu6() {
-//        String rep="";
-//        while (!"q".equals(rep)) {
-//            try {
-//                rep= c.nextLine();
-//                if ("o".equals(rep))
-//                        sense.onOff();
-//                else if ("s".equals(rep))
-//                        System.out.println("status :"+sense.getStatus());
-//                else if ("v".equals(rep))
-//                        System.out.println("value :"+sense.getValue());
-//                else System.out.println("q: quitter - o: switch - s: status - v: value");
-//            } catch (SensorNotActivated ex) {
-//                Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-//            }
-//        }
-//    }
 }
